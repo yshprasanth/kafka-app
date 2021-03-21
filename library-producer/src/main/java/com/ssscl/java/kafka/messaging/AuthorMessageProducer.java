5df08f0a63c2b3442a55f67a2ee6e7d60f219caa
Yalamanchili Sri Hari Prasanth <yshprasanth@gmail.com>
2021-03-21 11:25:35 +0000

    Added retry with resilience4j library

--- library-producer/src/main/java/com/ssscl/java/kafka/messaging/AuthorMessageProducer.java
+++ library-producer/src/main/java/com/ssscl/java/kafka/messaging/AuthorMessageProducer.java
@@ -1,7 +1,10 @@
 package com.ssscl.java.kafka.messaging;
 
 import com.ssscl.kafka.common.Author;
-import com.ssscl.kafka.common.Book;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
+import io.github.resilience4j.retry.RetryRegistry;
+import org.apache.kafka.common.errors.AuthorizationException;
 import org.apache.kafka.common.errors.TimeoutException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,16 +12,22 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.time.Duration;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 @Component
 @DependsOn("authorKafkaTemplate")
 public class AuthorMessageProducer implements Runnable {
+
+    private static final long THREAD_WAIT_TIME_MILLS = 2000l;
 
     @Autowired
     private KafkaTemplate<String, Author> authorKafkaTemplate;
@@ -26,16 +35,30 @@ public class AuthorMessageProducer implements Runnable {
     @Autowired
     private KafkaTemplate<String, Author> authorKafkaTemplateWithRetry;
 
-    @Value("${library.kafka.retries:3}")
-    private int retries;
+    @Value("${library.kafka.retry.maxAttempts}")
+    private int retryMaxAttempts = 3;
+
+    @Value("${library.kafka.retry.waitDuration}")
+    private long retryWaitDuration = 10;
+
+    @Value("${library.kafka.retry.resultWaitDuration}")
+    private long resultWaitDuration = 60;
 
     private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     private final static String AUTHOR_NAME_PREFIX = "Mr.";
     private final static String AUTHOR_KEY_PREFIX = "Key_";
-    private final static long THREAD_WAIT_TIME_MILLS = 2000l;
+
+    private final Retry retry;
 
     public AuthorMessageProducer() {
+        RetryConfig config = RetryConfig.custom().
+                maxAttempts(retryMaxAttempts).
+                waitDuration(Duration.of(retryWaitDuration, SECONDS)).
+                retryExceptions(TimeoutException.class, AuthorizationException.class).
+                build();
+        RetryRegistry registry = RetryRegistry.of(config);
+        retry = registry.retry("authorKafkaTemplate", config);
     }
 
     public void start() {
@@ -44,33 +67,49 @@ public class AuthorMessageProducer implements Runnable {
 
     @Override
     public void run() {
-        sendWithRetry();
-    }
-
-    public void sendWithRetry() {
-        send(authorKafkaTemplateWithRetry, true, 0);
-    }
-
-    public void sendWithoutRetry() {
-        send(authorKafkaTemplate, false, 0);
-    }
-
-    public void send(KafkaTemplate kafkaTemplate, boolean isRetry, int retryIndex) {
-        while (true) {
-            final long currentMillis = System.currentTimeMillis();
-            final String key = AUTHOR_KEY_PREFIX + currentMillis;
-
-            final Author author = new Author();
-            author.setFName(AUTHOR_NAME_PREFIX + currentMillis);
-
-            kafkaPublish(kafkaTemplate, key, author, isRetry, retryIndex);
-
-            try {
-                Thread.sleep(THREAD_WAIT_TIME_MILLS);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        while(true) {
+            sendWithRetryConfig();
         }
+    }
+
+    public void sendWithRetryConfig() {
+        Callable<ListenableFuture<SendResult<String, Author>>> kafkaPublisherCallable = Retry.decorateCallable(retry, () -> {
+            System.out.println("trying to invoke send again");
+            return send(authorKafkaTemplate, true, 0);
+        });
+
+        try {
+            ListenableFuture<SendResult<String, Author>> listenableFuture = kafkaPublisherCallable.call();
+            listenableFuture.get(resultWaitDuration, TimeUnit.SECONDS);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public ListenableFuture<SendResult<String, Author>> sendWithRetry() {
+        return send(authorKafkaTemplateWithRetry, true, 0);
+    }
+
+    public ListenableFuture<SendResult<String, Author>> sendWithoutRetry() {
+        return send(authorKafkaTemplate, false, 0);
+    }
+
+    private ListenableFuture<SendResult<String, Author>> send(KafkaTemplate kafkaTemplate, boolean isRetry, int retryIndex) {
+        final long currentMillis = System.currentTimeMillis();
+        final String key = AUTHOR_KEY_PREFIX + currentMillis;
+
+        final Author author = new Author();
+        author.setFName(AUTHOR_NAME_PREFIX + currentMillis);
+
+        ListenableFuture<SendResult<String, Author>> listenableFuture =
+                kafkaPublish(kafkaTemplate, key, author, isRetry, retryIndex);
+
+        try {
+            Thread.sleep(THREAD_WAIT_TIME_MILLS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return listenableFuture;
     }
 
     public void handleFailure(KafkaTemplate kafkaTemplate, String key, Author author, boolean isRetry, int retryIndex,
@@ -91,9 +130,8 @@ public class AuthorMessageProducer implements Runnable {
 
     }
 
-    private void kafkaPublish(KafkaTemplate kafkaTemplate, String key, Author author, boolean isRetry, int retryIndex) {
+    private ListenableFuture<SendResult<String, Author>> kafkaPublish(KafkaTemplate kafkaTemplate, String key, Author author, boolean isRetry, int retryIndex) {
         final ListenableFuture<SendResult<String, Author>> future = kafkaTemplate.sendDefault(key, author);
-
         System.out.println("Sending message=[" + author + "]" + (isRetry ? " with retry: " + retryIndex : ""));
         future.addCallback(new ListenableFutureCallback<>() {
             public void onSuccess(SendResult<String, Author> result) {
@@ -107,6 +145,7 @@ public class AuthorMessageProducer implements Runnable {
                 handleFailure(kafkaTemplate, key, author, isRetry, retryIndex, ex);
             }
         });
+        return future;
     }
 
     public void terminate() {
